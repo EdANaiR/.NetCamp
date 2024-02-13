6ed0926acbda73c792ab720fdf03962ba099da6a
Eda Nur Nair <93126814+EdANaiR@users.noreply.github.com>
2024-02-13 22:48:01 +0300

    Lesson 1

---
+++ intro/intro/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using intro.Bussines;
+using intro.Entities;
+
+Console.WriteLine("Hello, World!");
+
+
+bool isAuthenticated = false;
+
+//condition
+if (isAuthenticated)                // isAuthenticated==true ile aynı  
+{
+    Console.WriteLine("Buton --> Hoşgeldin Kullanıcı");
+}
+else
+{
+    Console.WriteLine("Buton-->Sisteme giriş yap");   
+}
+
+string[] loans = { "Kredi 1", "Kredi 2", "Kredi 3", "Kredi 4", "Kredi 5", "Kredi 6" }; //Db'den gelir
+
+for (int i =0; i<loans.Length; i++)                //i loans uzunluğundan büyük olana kadar döngüye devam et
+{
+    Console.WriteLine(loans[i]);                   // index değerlerini yazdırır.
+}
+
+
+
+
+CourseManager coursemanager = new CourseManager();
+Course[] courses2 = coursemanager.GetAll();
+
+for (int i = 0; i < courses2.Length; i++)
+{
+    Console.WriteLine(courses2[i].Name + "/" + courses2[i].Price);
+}
